2ee49d6e8e5431a1996472943de74d5277629bdc
sevenlee <345413763@qq.com>
2020-06-01 02:47:14 +0800

    feat:代码提交

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d');
 const utils = <HTMLCanvasElement>document.getElementById("mapBlock");
 const utilCtx = utils.getContext('2d');
 const img: HTMLImageElement = new Image();
-let mapBlockData: ImageData, mapData, blockWidth: number, blockHeight: number, pageIndex = 0, imgNum = filesJson.tilesets.length;
+let mapBlockData: ImageData, mapUtilData, mapData, blockWidth: number, blockHeight: number, pageIndex = 0, imgNum = filesJson.tilesets.length, modeType = 1;
 let mapBlockArr: Array<any> = []
 setImg(0)
 img.onload = () => {
@@ -17,24 +17,18 @@ img.onload = () => {
     utilCtx.textBaseline = "middle"
     utilCtx.font = "28px Arial"
     utilCtx.drawImage(img, 0, 0)
-    mapData = utilCtx.getImageData(0, 0, utils.width, utils.height)
+    mapUtilData = utilCtx.getImageData(0, 0, utils.width, utils.height)
     if (!mapBlockArr) {
         mapBlockArr = []
         for (let i = 0; i < utils.height / BLOCK_WIDTH; i++) {
             mapBlockArr.push(new Array(utils.width / BLOCK_WIDTH).fill(0))
         }
     }
-    setMapJson()
+    if (!modeType) {
+        setMapJson()
+    }
 }
-utils.onmouseup = (e) => {
-    let { left, top } = utils.getBoundingClientRect()
-    let x = e.clientX - left, y = e.clientY - top;
-    let bx = Math.floor(x / BLOCK_WIDTH),
-        by = Math.floor(y / BLOCK_WIDTH);
-    mapBlockArr[by][bx] = ++mapBlockArr[by][bx] < 3 ? mapBlockArr[by][bx] : 0
-    utilCtx.putImageData(mapData, 0, 0)
-    setMapJson()
-}
+
 function setMapJson() {
     utilCtx.beginPath();
     mapBlockArr.forEach((element, cy) => {
@@ -51,6 +45,18 @@ function setImg(p) {
     mapBlockArr = mapsJson[filesJson.tilesets[p].split('.')[0]]
     img.src = './../static/img/tilesets/' + filesJson.tilesets[p];
 }
+utils.onmousedown = (e) => {
+    let { left, top } = utils.getBoundingClientRect()
+    let x = e.clientX - left, y = e.clientY - top;
+    getRectBlock(x, y, x, y)
+    utils.onmousemove = (e) => {
+        let x1 = e.clientX - left, y1 = e.clientY - top;
+        getRectBlock(x, y, x1, y1)
+    }
+    utils.onmouseup = (e) => {
+        utils.onmousemove = null
+    }
+}
 document.getElementById('createJson').addEventListener("click", () => {
     console.log(JSON.stringify(mapBlockArr));
 })
@@ -66,28 +72,64 @@ document.getElementById('nextPage').addEventListener("click", () => {
         setImg(++pageIndex)
     }
 })
-// utils.onmousedown = (e) => {
-//     let { left, top } = utils.getBoundingClientRect()
-//     let x = e.clientX - left, y = e.clientY - top;
-//     getRectBlock(x, y, x, y)
-//     utils.onmousemove = (e) => {
-//         let x1 = e.clientX - left, y1 = e.clientY - top;
-//         getRectBlock(x, y, x1, y1)
-//     }
-//     utils.onmouseup = (e) => {
-//         utils.onmousemove = null
-//     }
-// }
+document.getElementById('changeMode').addEventListener("click", () => {
+    utilCtx.putImageData(mapUtilData, 0, 0)
+    if (modeType) {
+        modeType = 0
+        utils.onmousedown = null
+        setMapJson()
+        utils.onmouseup = (e) => {
+            let { left, top } = utils.getBoundingClientRect()
+            let x = e.clientX - left, y = e.clientY - top;
+            let bx = Math.floor(x / BLOCK_WIDTH),
+                by = Math.floor(y / BLOCK_WIDTH);
+            mapBlockArr[by][bx] = ++mapBlockArr[by][bx] < 3 ? mapBlockArr[by][bx] : 0
+            utilCtx.putImageData(mapUtilData, 0, 0)
+            setMapJson()
+        }
+    } else {
+        modeType = 1
+        utils.onmousedown = (e) => {
+            let { left, top } = utils.getBoundingClientRect()
+            let x = e.clientX - left, y = e.clientY - top;
+            getRectBlock(x, y, x, y)
+            utils.onmousemove = (e) => {
+                let x1 = e.clientX - left, y1 = e.clientY - top;
+                getRectBlock(x, y, x1, y1)
+            }
+            utils.onmouseup = (e) => {
+                utils.onmousemove = null
+            }
+        }
+    }
+})
+function mousemove() {
+    canvas.onmousemove = (e) => {
+        let { left, top } = canvas.getBoundingClientRect()
+        let x = e.clientX - left, y = e.clientY - top;
+        let bx = Math.floor(x / BLOCK_WIDTH) * BLOCK_WIDTH,
+            by = Math.floor(y / BLOCK_WIDTH) * BLOCK_WIDTH;
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.putImageData(mapData, 0, 0)
+        ctx.beginPath();
+        ctx.rect(bx, by, blockWidth, blockHeight);
+        ctx.stroke()
+    }
+}
+mousemove()
+mapData = ctx.getImageData(0, 0, canvas.width, canvas.height)
 canvas.onmousedown = (e) => {
     let { left, top } = canvas.getBoundingClientRect()
     let x = e.clientX - left, y = e.clientY - top;
+    ctx.putImageData(mapData, 0, 0)
     drawMap(x, y)
     canvas.onmousemove = (e) => {
         let x1 = e.clientX - left, y1 = e.clientY - top;
         drawMap(x1, y1)
     }
     canvas.onmouseup = (e) => {
-        canvas.onmousemove = null
+        mapData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+        mousemove()
     }
 }
 function drawMap(x, y) {
@@ -100,8 +142,8 @@ function drawMap(x, y) {
     ct.putImageData(mapBlockData, 0, 0)
     let image: HTMLImageElement = new Image()
     image.src = c.toDataURL('image/png')
-
     ctx.drawImage(image, bx, by)
+    mapData = ctx.getImageData(0, 0, canvas.width, canvas.height)
 }
 
 function getRectBlock(x, y, x1, y1) {
@@ -109,8 +151,7 @@ function getRectBlock(x, y, x1, y1) {
         by = Math.floor(y / BLOCK_WIDTH) * BLOCK_WIDTH,
         bx1 = Math.ceil(x1 / BLOCK_WIDTH) * BLOCK_WIDTH,
         by1 = Math.ceil(y1 / BLOCK_WIDTH) * BLOCK_WIDTH;
-    utilCtx.clearRect(0, 0, utils.width, utils.height)
-    utilCtx.putImageData(mapData, 0, 0)
+    utilCtx.putImageData(mapUtilData, 0, 0)
     utilCtx.beginPath();
     utilCtx.rect(bx - 2, by - 2, bx1 - bx + 4, by1 - by + 4);
     utilCtx.stroke();
